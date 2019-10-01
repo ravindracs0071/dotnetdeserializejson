7c5cd566e7dce99f2ec7d22ca1c79ad654c828a7
Ravindra Prajapati <ravindracs0071@gmail.com>
2019-10-01 18:23:58 +0530

    Help One

---
+++ AjaxApp/Controllers/HomeController.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace AjaxApp.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Index(List<int> elements)
+        {
+
+            return Json(elements.Count, JsonRequestBehavior.AllowGet);
+        }
+
+        [HttpPost]
+        public JsonResult CallCounter(List<Int64> elements)
+        {
+
+            return Json(elements.Count, JsonRequestBehavior.AllowGet);
+        }
+    }
+}
---
+++ AjaxApp/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(AjaxApp.Startup))]
+namespace AjaxApp
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
